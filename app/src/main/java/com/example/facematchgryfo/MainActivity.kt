4f5859f4754e8b3d9e779e9e529bc3b9f648af22
GuilhermeXav1er <guixavier5953@gmail.com>
2024-05-12 21:17:13 -0300

    salvando nos arquivos

--- app/src/main/java/com/example/facematchgryfo/MainActivity.kt
+++ app/src/main/java/com/example/facematchgryfo/MainActivity.kt
@@ -10,7 +10,23 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Environment
 import android.widget.Toast
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraProvider
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.OutputFileOptions
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +36,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val CAMERA_PERMISSION_CODE = 100
+    private lateinit var imageCapture: ImageCapture
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var camera: Camera
+    private lateinit var cameraSelector: CameraSelector
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +63,10 @@ class MainActivity : AppCompatActivity() {
         } else {
             // Permission has already been granted
             openCamera()
+        }
+
+        mainBinding.captureImageButton.setOnClickListener{
+            takePhoto()
         }
     }
 
@@ -70,5 +95,105 @@ class MainActivity : AppCompatActivity() {
     private fun openCamera() {
         // Code to open the camera
         Toast.makeText(this, "permissao garantida", Toast.LENGTH_SHORT).show()
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            cameraProvider = cameraProviderFuture.get()
+            bindCameraUserCases()
+        }, ContextCompat.getMainExecutor(this))
     }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = maxOf(width, height).toDouble() / minOf(width, height)
+        return if (abs(previewRatio - 4.0 / 3.0) <= abs(previewRatio - 16.0 / 9.0)) {
+            AspectRatio.RATIO_4_3
+        }
+        else {
+            AspectRatio.RATIO_16_9
+        }
+    }
+    private fun bindCameraUserCases() {
+        val screenAspectRadio = aspectRatio(
+            mainBinding.previewView.width,
+            mainBinding.previewView.height
+        )
+        val rotation = mainBinding.previewView.display.rotation
+
+
+        val resolutionSelector = ResolutionSelector.Builder()
+            .setAspectRatioStrategy(
+                AspectRatioStrategy(
+                    screenAspectRadio,
+                    AspectRatioStrategy.FALLBACK_RULE_AUTO
+                )
+            ).build()
+
+        val preview = Preview.Builder()
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setSurfaceProvider(mainBinding.previewView.surfaceProvider)
+            }
+
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+
+        cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(lensFacing)
+            .build()
+
+        try {
+            cameraProvider.unbindAll()
+
+            camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, imageCapture
+            )
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+
+    private fun takePhoto(){
+        val imageFolder = File(
+            Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES), "Images"
+        )
+        if (!imageFolder.exists()) {
+            imageFolder.mkdirs()
+        }
+
+        val fileName = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault())
+            .format(System.currentTimeMillis()) + ".jpg"
+        val imageFile = File(imageFolder, fileName)
+        val outputOption = OutputFileOptions.Builder(imageFile).build()
+
+        imageCapture.takePicture(
+            outputOption,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val message = "Photo Capture Succeeded: ${outputFileResults.savedUri}"
+                    Toast.makeText(
+                        this@MainActivity,
+                        message,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        exception.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            }
+        )
+    }
+
+
 }
